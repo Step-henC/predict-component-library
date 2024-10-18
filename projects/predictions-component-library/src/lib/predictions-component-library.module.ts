03cf80a4d48351aa1d7a5c80d4702e5755172276
Step-henC <stevecunningham@outlook.com>
2024-10-18 07:56:31 -0500

    added alerts

--- projects/predictions-component-library/src/lib/predictions-component-library.module.ts
+++ projects/predictions-component-library/src/lib/predictions-component-library.module.ts
@@ -4,6 +4,7 @@ import { TableComponent } from "./PredictTable/table.component";
 import { LoadingSpinnerComponent } from "./loadingSpinner/loadingspinner.component";
 import { NgClass } from "@angular/common";
 import { PredictButtonComponent } from "./button/predictbutton.component";
+import { AlertToastComponent } from "./alerttoast/alerttoast.component";
 
 
 @NgModule({
@@ -11,14 +12,16 @@ import { PredictButtonComponent } from "./button/predictbutton.component";
     TableComponent,
     HeaderComponent,
     LoadingSpinnerComponent,
-    PredictButtonComponent
+    PredictButtonComponent,
+    AlertToastComponent
   ],
   imports: [NgClass], // since components are non-standalone we need this
   exports: [
     TableComponent,
     HeaderComponent,
     LoadingSpinnerComponent,
-    PredictButtonComponent
+    PredictButtonComponent,
+    AlertToastComponent
   ]
 })
 
--- projects/predictions-component-library/src/public-api.ts
+++ projects/predictions-component-library/src/public-api.ts
@@ -1,5 +1,6 @@
 /*
  * Public API Surface of predictions-component-library
+   Needed for builds to succeed. All Components must be publicly accessible
  */
 
 export * from './lib/predictions-component-library.service';
@@ -9,3 +10,4 @@ export * from './lib/PredictTable/table.component'
 export * from './lib/predictions-component-library.module'
 export * from './lib/loadingSpinner/loadingspinner.component'
 export * from './lib/button/predictbutton.component'
+export * from './lib/alerttoast/alerttoast.component'
